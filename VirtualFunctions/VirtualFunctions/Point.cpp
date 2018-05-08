634f45c6ba62a3d1480304865b1e606eefdf5753
Dan Singer <dsinger.sax@gmail.com>
2018-05-08 16:43:07 -0400

    Complete section quiz on virtual functions

---
+++ VirtualFunctions/VirtualFunctions/Point.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include "Point.h"
+
+std::ostream& operator<<(std::ostream &out, const Point &p)
+{
+	out << "Point(" << p.m_x << ", " << p.m_y << ", " << p.m_z << ")";
+	return out;
+}
